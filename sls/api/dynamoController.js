d8147aa0256594d646643b615fbd0078742be895
ngonzaa27 <ngonzaa27@gmail.com>
2024-02-05 20:00:33 +0000

    Minor change

--- sls/api/dynamoController.js
+++ sls/api/dynamoController.js
@@ -1,8 +1,11 @@
 module.exports = {
   gett: function (req, res) {
+    const {userId, name} = req.body
     return res.status(200).json({ 
-      "id": 1 , 
-      "name": "nicolas" 
+      "id": 1 ,
+      "id1": userId,
+      "name": "nicolas",
+      "name1": name
     });    
   }
 }
